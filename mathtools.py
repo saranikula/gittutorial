1d39688cb05be2aff060a2d72ea42932840c8352
saranikula <sara.nikula@student.oulu.fi>
2018-12-14 13:59:49 +0200

    commit from fibonacci

--- mathtools.py
+++ mathtools.py
@@ -27,6 +27,8 @@ def fib(n):
     else:
         return fib(n-1)+fib(n-2)
 		
+		
+		
 def arithmetic(a, difference, n):
     '''Calculates the sum of a arithmetic serie of n elements.
        An arithmetic sequence is of the form: a, a+d, a+2d, a+3d,...
